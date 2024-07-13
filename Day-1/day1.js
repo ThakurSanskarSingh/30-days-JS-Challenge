a0c35ac2efde8d13a224eb99cd3e58230e768af6
Sanskar Singh <sanskarsinghgamerz01@gmail.com>
2024-07-13 19:03:04 +0530

    day 1

---
+++ Day-1/day1.js
@@ -0,0 +1,39 @@
+
+var num = 7
+console.log(num)
+
+let str  = "String"
+console.log(str)
+
+const boolean = true
+console.log(boolean)
+
+
+var number = 88
+console.log(typeof(number))
+
+let check = 5
+
+console.log(check)
+
+check  = 74
+console.log(check)
+
+const check2 = 7
+console.log(check2)
+
+// check2 = 8
+// console.log(check2)
+//we can not reasdign value to a const vairable
+
+
+let initial = "1stvalue"
+console.log(initial)
+initial ="final val"
+
+const first = "first const vlaue"
+ first = "second const vlaue"
+
+
+ console.log(initial)
+//  console.log(first)
