4dd1978d707bed65655ed62b58fdebf8c849c8aa
Sanskar Singh <work.sanskarr@gmail.com>
2024-07-24 22:41:39 +0530

    Day 7,8,9

--- Day-7/Day-7.js
+++ Day-7/Day-7.js
@@ -11,3 +11,19 @@ let book2 = {"year" : "1947"}
 Object.assign(book,book2)
 console.log(book)
 
+let user = {
+    name : "sanskar",
+    age : "18",
+    caste : "Thakur",
+    
+ thisMethod() {
+    console.log(this.name)
+}
+}
+
+
+for(let key in user){
+    console.log(key)
+    // console.log(user[key])
+}
+user.thisMethod()
---
+++ Day8/Day-8.js
@@ -0,0 +1,6 @@
+let name = "sanskar"
+let age = 18
+console.log(`His name is ${name} and age is ${age}`)
+
+
+//arrray destructing
